a458c731d673e6f2d0cb9be15e02d7f140dcb548
iguest0001 <iguest0001@gmail.com>
2026-02-21 17:40:03 +0530

    fix: text cursor blink permanently band - _app.js mein !important se user-select none

--- frontend/user-panel/pages/_app.js
+++ frontend/user-panel/pages/_app.js
@@ -1,9 +1,25 @@
 import '../styles/globals.css';
 import { ToastProvider } from '../contexts/ToastContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
         <ToastProvider>
+            <Head>
+                <style>{`
+                    *, *::before, *::after {
+                        -webkit-user-select: none !important;
+                        -moz-user-select: none !important;
+                        -ms-user-select: none !important;
+                        user-select: none !important;
+                    }
+                    input, textarea, [contenteditable="true"] {
+                        -webkit-user-select: text !important;
+                        -moz-user-select: text !important;
+                        user-select: text !important;
+                    }
+                `}</style>
+            </Head>
             <Component {...pageProps} />
         </ToastProvider>
     );
